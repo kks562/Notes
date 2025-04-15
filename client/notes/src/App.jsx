68012ad87745a0015c13e280971b60fd8879443d
kks562 <kavin.krishnas.562@gmail.com>
2025-04-15 19:52:46 +0530

    noters

--- client/notes/src/App.jsx
+++ client/notes/src/App.jsx
@@ -1,55 +1,61 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  const [notes,setNotes]=useState([]);
-  const [note,setNote]=useState({title:"",content:""});
-  useEffect(()=>{
-    axios.get("http://localhost:3000/api/notes").then((res)=>{
-            setNotes(res.data);
-    })
-  },[]);
-  const addnote=()=>{
-    axios.post("http://localhost:3000/api/notes",note).then((res)=>{
-      
-      setNotes([...notes,res.data]);
-    });
-    setNote({title:"",content:""});
-  }
-  const deleteNote=(id)=>{
-    axios.delete(`http://localhost:3000/api/notes/${id}`).then((res)=>{
-       setNotes(notes.filter((n)=>n._id!==id));
-    });
-  }
+  const [notes, setNotes] = useState([]);
+  const [note, setNote] = useState({ title: "", content: "" });
+
+  useEffect(() => {
+    axios.get("https://notes-1-nxrg.onrender.com/api/notes")
+      .then((res) => {
+        setNotes(res.data);
+      });
+  }, []);
+
+  const addNote = () => {
+    axios.post("https://notes-1-nxrg.onrender.com/api/notes", note)
+      .then((res) => {
+        setNotes([...notes, res.data]);
+      });
+    setNote({ title: "", content: "" });
+  };
+
+  const deleteNote = (id) => {
+    axios.delete(`https://notes-1-nxrg.onrender.com/api/notes/${id}`)
+      .then(() => {
+        setNotes(notes.filter((n) => n._id !== id));
+      });
+  };
 
   return (
     <div>
-      <h1>NOTER</h1>
-      <br>
-      </br>
-      <input placeholder='Enter Title' value={note.title} onChange={(e)=>setNote({...note,title:e.target.value})}></input>
-      <hr></hr>
-      <input placeholder='Enter Content' value={note.content} onChange={(e)=>setNote({...note,content:e.target.value})}></input>
-      <br></br>
-      <br></br>
-      <br></br>
-       <button onClick={addnote}>Add Note</button>
+      <h1>NOTERS</h1>
+      <br />
+      <input
+        placeholder="Enter Title"
+        value={note.title}
+        onChange={(e) => setNote({ ...note, title: e.target.value })}
+      />
+      <hr />
+      <input
+        placeholder="Enter Content"
+        value={note.content}
+        onChange={(e) => setNote({ ...note, content: e.target.value })}
+      />
+      <br /><br /><br />
+      <button onClick={addNote}>Add Note</button>
 
-       <ul>
-        {
-          notes.map((n)=>(
-            <li key={n._id}>
-              <h2>{n.title}</h2>
-              <h4>{n.content}</h4>
-              <button onClick={()=>deleteNote(n._id)}>Delete Note</button>
-            </li>
-          ))
-        }
-       </ul>
+      <ul>
+        {notes.map((n) => (
+          <li key={n._id}>
+            <h2>{n.title}</h2>
+            <h4>{n.content}</h4>
+            <button onClick={() => deleteNote(n._id)}>Delete Note</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
